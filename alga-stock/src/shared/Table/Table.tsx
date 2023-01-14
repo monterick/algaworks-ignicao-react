7cca312c734677a21bf87484d943cef72ee0dfde
monterick <rick_-26@hotmail.com>
2023-01-14 14:26:56 -0300

    commit

--- alga-stock/src/shared/Table/Table.tsx
+++ alga-stock/src/shared/Table/Table.tsx
@@ -1,29 +1,29 @@
 import React from "react";
-//import './Table.scss'
+import './Table.scss'
 
 const Table = () =>{
     return(
-        <table>
+        <table className="AppTable">
           <thead>
             <th>Produto</th>
             <th>Preço</th>
-            <th>Estoque</th>
+            <th className="right">Estoque</th>
           </thead>
           <tbody>
             <tr>
                 <td>Maca</td>
                 <td>5.51</td>
-                <td>45</td>
+                <td className="right">45</td>
             </tr>
             <tr>
                 <td>Banana</td>
                 <td>3.4</td>
-                <td>100</td>
+                <td className="right">100</td>
             </tr>
             <tr>
                 <td>Melao</td>
                 <td>5.52</td>
-                <td>80</td>
+                <td className="right">80</td>
             </tr>
           </tbody>
         </table>

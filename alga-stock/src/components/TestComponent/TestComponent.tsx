8c5f33971fa7d43e4001edd56fbce852f1ed9cb2
monterick <rick_-26@hotmail.com>
2023-01-13 23:10:12 -0300

    commit

---
+++ alga-stock/src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TestComponent.css"
+
+const TestComponent = () => (
+  <div className="test-component">Test Component</div>
+  )
+
+export default TestComponent

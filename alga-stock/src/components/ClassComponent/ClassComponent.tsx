049cdd9476a59f0fb115e96a63a1dc528232c7b1
monterick <rick_-26@hotmail.com>
2023-01-14 00:00:19 -0300

    ciclo de vida

--- alga-stock/src/components/ClassComponent/ClassComponent.tsx
+++ alga-stock/src/components/ClassComponent/ClassComponent.tsx
@@ -3,12 +3,25 @@ import React from 'react'
 class ClassComponent extends React.Component<{name:String}>{
 
   
+    constructor(props: any){
+        super(props);
+        console.log("constructor")
+    }
 
     state = {
        name:"Mundo"
     }
 
+    componentDidMount(){
+        console.log("Did mount")
+    }
+
+    componentDidUpdate(){
+        console.log("update")
+    }
+
     render(){
+        console.log("render2")
         return(
             <div>
                 Component { this.props.name } Baseado em classe

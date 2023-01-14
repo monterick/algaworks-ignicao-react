8fd818f417679c48ab2d680fa38f99371fff185e
monterick <rick_-26@hotmail.com>
2023-01-14 13:21:13 -0300

    commit

--- alga-stock/src/shared/Table/Table.tsx
+++ alga-stock/src/shared/Table/Table.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import './Table.scss'
+//import './Table.scss'
 
 const Table = () =>{
     return(
         <table>
-
+          <thead>
+            <th>Produto</th>
+            <th>Preço</th>
+            <th>Estoque</th>
+          </thead>
+          <tbody>
+            <tr>
+                <td>Maca</td>
+                <td>5.51</td>
+                <td>45</td>
+            </tr>
+            <tr>
+                <td>Banana</td>
+                <td>3.4</td>
+                <td>100</td>
+            </tr>
+            <tr>
+                <td>Melao</td>
+                <td>5.52</td>
+                <td>80</td>
+            </tr>
+          </tbody>
         </table>
     )
 }
